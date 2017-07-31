a45758f167f4d28aece5d988e01317bb27a1781d
GIT Admin <ola.karlsson@bahnhof.se>
2017-07-31 02:01:53 +0200

    misc changes

--- src/Main.cpp
+++ src/Main.cpp
@@ -18,16 +18,27 @@ int main() {
 	Screen screen;
 
 	if (screen.init() == false) {
-		cout << "Error initializing SDL" << endl;
+		cout << "Error initialising SDL." << endl;
 	}
 
 	while (true) {
+
+		for (int y = 0; y < Screen::SCREEN_HEIGHT; y++) {
+			for (int x = 0; x < Screen::SCREEN_WIDTH; x++) {
+				screen.setPixel(x, y, 128, 0, 255);
+			}
+		}
+
+		screen.setPixel(400, 300, 255, 255, 255);
+
+		screen.update();
+
 		if (screen.ProcessEvent() == false) {
 			break;
 		}
-
 	}
 
 	screen.close();
+
 	return 0;
 }
--- src/Screen.h
+++ src/Screen.h
@@ -30,6 +30,8 @@ public:
 	bool init();
 	void close();
 	bool ProcessEvent();
+	void update();
+	void setPixel(int x, int y, Uint8 red, Uint8 green,Uint8 blue);
 
 	virtual ~Screen();
 };
